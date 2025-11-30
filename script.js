a2c8d35bb3a362f5c89af5e1b9c0c6fa0954c8ca
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-11-30 18:17:13 +0100

    refactor: rename parameter in getChefBirthday function for clarity

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-async function getChefBirthday(id){
-  const recipeResponse = await fetch(`https://dummyjson.com/recipes/${id}`);
+async function getChefBirthday(idRicetta){
+  const recipeResponse = await fetch(`https://dummyjson.com/recipes/${idRicetta}`);
   const recipe = await recipeResponse.json();
   const chefResponse = await fetch(`https://dummyjson.com/users/${recipe.userId}`);
   const chef = await chefResponse.json();
@@ -16,6 +16,6 @@ async function getChefBirthday(id){
 })();
 
 
-getChefBirthday(1)
-  .then(birthday => console.log("Data di nascita dello chef:", birthday))
-  .catch(error => console.error("Errore:", error.message));
+//getChefBirthday(1)
+  //.then(birthday => console.log("Data di nascita dello chef:", birthday))
+  //.catch(error => console.error("Errore:", error.message));
